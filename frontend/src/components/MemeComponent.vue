02401b4fdaecefecfd6fd40b5d85725df3354782
wei-coding <edward.yu0830@gmail.com>
2024-10-03 10:33:33 +0000

    first step of frontend

---
+++ frontend/src/components/MemeComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+    imageUrl: string;
+    link: string;
+}>();
+
+function copyLink() {
+    navigator.clipboard.writeText(props.link);
+}
+</script>
+
+<template>
+    <a v-on:click="copyLink">
+        <img :src="props.imageUrl" alt="範例圖片" class="image-item rounded">
+    </a>
+</template>
+
+<style scoped>
+a {
+    cursor: pointer;
+}
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<script setup type="ts">
+import MemeComponent from '../components/MemeComponent.vue';
+import UploadComponent from '../components/UploadComponent.vue';
+import { ref } from 'vue';
+
+var memeList = ref([]);
+var search = ref("");
+
+for (let i=0; i<9; i++) {
+    memeList.value.push({
+        link: "123",
+        imageUrl: "https://via.assets.so/img.jpg?w=300&h=300"
+    })
+}
+
+function isPageScrolledToBottom() {
+    const visibleHeight = window.innerHeight;
+    const scrollHeight = document.documentElement.scrollTop || document.body.scrollTop;
+    const totalHeight = document.documentElement.scrollHeight;
+    
+    if (totalHeight <= visibleHeight + scrollHeight + 1) {
+        for (let i=0; i<9; i++) {
+            memeList.value.push({
+                link: "123",
+                imageUrl: "https://via.assets.so/img.jpg?w=300&h=300"
+            })
+        }
+    }
+}
+
+addEventListener("scroll", isPageScrolledToBottom);
+
+</script>
+
+<template>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">MEMEs Search</a>
+            </div>
+        </nav>
+
+        <div class="container mt-4">
+            <div class="row justify-content-center mb-4">
+                <div class="col-md-6">
+                    <input type="text" class="form-control" placeholder="搜尋..." v-model="search">
+                </div>
+            </div>
+
+            <div class="row justify-content-center mb-4">
+                <div class="col-md-6 text-center">
+                    <UploadComponent/>
+                </div>
+            </div>
+
+            <div class="image-grid">
+                <MemeComponent v-for="meme in memeList" :link="meme.link" :imageUrl="meme.imageUrl" />
+            </div>
+        </div>
+    </div>
+</template>
